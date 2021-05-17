e1b9c00a7c20669be5ec2cc32bf11e860b610372
Matheus Diegues <dmosken2015@gmail.com>
2021-05-17 17:12:55 -0300

    comentarios movidos de lugar

--- projeto2/routes/web.php
+++ projeto2/routes/web.php
@@ -1,28 +1,7 @@
 <?php
-
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProdutosController;
-/*
-
-
-Route::get('/', function () {
-    return view('home');
-});
-composer create-project --prefer-dist laravel/lumen nomedoprojeto
-laravel new nomedoprojeto
-
-composer create-project laravel/laravel nome-projeto --prefer-dist
-
-criando server
-
-php -S localhost:8000 -t public
-
-Route::get('/', function (){
-    return view('home');
-});
-*/
-
 
 Route::get('/', [HomeController::class, 'index']);
 Route::get('/Produtos', [ProdutosController::class, 'index']);
@@ -44,3 +23,24 @@ Route::get('/How-It-Works', function (){
 Route::get('/produtos/{nomedoproduto}', function ($nomedoproduto){
     echo '<h1>Pagina do Produto: </h1>'.$nomedoproduto;
 });
+
+
+/*
+
+
+Route::get('/', function () {
+    return view('home');
+});
+composer create-project --prefer-dist laravel/lumen nomedoprojeto
+laravel new nomedoprojeto
+
+composer create-project laravel/laravel nome-projeto --prefer-dist
+
+criando server
+
+php -S localhost:8000 -t public
+
+Route::get('/', function (){
+    return view('home');
+});
+*/
