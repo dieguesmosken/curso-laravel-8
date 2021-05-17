464cf69224c8c736cf5fa563ac05b75e1fa4decc
Matheus Diegues <dmosken2015@gmail.com>
2021-05-17 15:58:35 -0300

    adm commits

--- projeto2/resources/views/admin/adm.blade.php
+++ projeto2/resources/views/admin/adm.blade.php
@@ -5,6 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Admin</title>
+    
 </head>
 <body>
     <h1>teste de page voltar para o <a href="/">Index</a></h1>
