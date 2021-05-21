b1a24250e2bb89049d852b77f6dcc2912cb84b28
Matheus Diegues <dmosken2015@gmail.com>
2021-05-21 16:36:49 -0300

    alterações

--- projeto2/resources/views/login/login.blade.php
+++ projeto2/resources/views/login/login.blade.php
@@ -1,9 +1,6 @@
 <?php
 session_start();
-
-
-?>
-	
+?>	
 <!DOCTYPE html>
 <html lang="pt-br">
   <head>
@@ -14,9 +11,9 @@ session_start();
     <title>Login Crud</title>
 
     <!-- Bootstrap -->
-    <link href="css/bootstrap.css" rel="stylesheet">
+    <link href="../css/bootstrap.css" rel="stylesheet">
 	 <!-- Estilos Customizados -->
-    <link href="css/signin.css" rel="stylesheet">
+    <link href="../css/signin.css" rel="stylesheet">
     <!--[bootstrap cdn]>
       <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
       <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
@@ -24,7 +21,7 @@ session_start();
 	<style type="text/css">
 		body{
       /*background-image: url("imagens/423148.jpg")*/
-      background-image: url("imagens/543102.jpg")
+      background-image: url("../imagens/543102.jpg")
       /*background-image: url("imagens/543917.jpg")*/
       
 		}
@@ -37,7 +34,9 @@ session_start();
 
 
 ?>
-      <form class="form-signin" method="POST" action="valida.php">
+      <form class="form-signin" method="POST" action="/login/validacao-user">
+      @csrf
+      
         <h2 class="form-signin-heading">Faça o Login: </h2>
         <input type="text" disabled class="form-control" value="<?php echo $_SESSION['msg'] ?>">
         <label for="email" class="sr-only">E-mail</label>
@@ -54,8 +53,8 @@ session_start();
 	
 	
     <!-- jQuery (necessario para plugins Bootstrap's JavaScript ) -->
-    <script src="js/jquery.min.js"></script>
+    <script src="../js/jquery.min.js"></script>
     <!-- Inclua todos os plugins compilados (abaixo) ou inclua arquivos individuais conforme necessário -->
-    <script src="js/bootstrap.min.js"></script>
+    <script src="../js/bootstrap.min.js"></script>
   </body>
 </html>
